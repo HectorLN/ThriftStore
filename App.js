ee153bab728e4f431b6d718ba9fc704dd29d03c2
HectorLN <hector.lucero193@tectijuana.edu.mx>
2023-11-01 20:38:53 -0700

    Initialized login

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput style={styles.textInput} placeholder="Correo" />
+      <TextInput style={styles.textInput} placeholder="Contraseña" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#EFF6EE",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#090C02",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#808080",
+    padding: 10,
+    paddingStart: 20,
+    width: "80%",
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: "#EDF2F4",
   },
 });

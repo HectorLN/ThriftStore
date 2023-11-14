6504f6f1a3667824854c07909cf3954498fd49df
HectorLN <hector.lucero193@tectijuana.edu.mx>
2023-11-13 19:20:23 -0800

    changes

---
+++ views/home.js
@@ -0,0 +1,19 @@
+import react from "react";
+import { StatusBar } from "expo-status-bar";
+import { Image } from "expo-image";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+} from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
---
+++ views/register.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { View } from "react-native-web";
+
+const Register = () => {
+  return (
+    <View>
+      <Text>Register</Text>
+    </View>
+  );
+};
+
+export default Register;
